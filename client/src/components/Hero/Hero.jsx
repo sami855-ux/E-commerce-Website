39348c1b7a0748c0fd00e233c5bd49831c77b872
sami855-ux <samitale56@outlook.com>
2025-01-15 10:07:40 +0500

    Responisivness for the hero section is started

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,25 +1,70 @@
+import { useState } from "react"
+import { motion } from "framer-motion"
+
+import heroImage from "../../assets/slider-layer-1.png"
+import { HiArrowRight } from "react-icons/hi"
+
 export default function Hero() {
+  const [isHover, setIsHover] = useState(false)
+
   return (
-    <div className="w-full h-screen flex items-center justify-center px-48">
-      <section className="w-1/3 h-full flex justify-end flex-col pb-10">
+    <div className="w-full min-h-screen flex  items-center justify-center md:px-36 lg:px-48 px-5">
+      <section className="w-2/3 md:w-1/3 h-full flex justify-end flex-col lg:pt-16">
         <p className="font-Poppins capitalize tracking-wider text-lightGreen">
           We are expert team
         </p>
 
-        <h2 className="font-bold text-2xl mt-3 py-2">
-          We Sell products that makes people&apos;s{" "}
+        <h2 className="font-bold text-lg md:text-3xl mt-3 py-2">
+          We sell products that makes people&apos;s{" "}
           <span className="text-lightGreen">lives</span> easier & better.
         </h2>
-        <p className="font-light text-sm">
+        <p className="font-light text-xs md:text-sm">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta
           accusamus reiciendis doloremque dolor eligendi inventore?
         </p>
 
-        <button className="w-40 h-10 bg-[#654321] text-white my-5 text-sm font-semibold font-Poppins">
+        <div
+          className="w-44 md:w-56 flex justify-center items-center bg-gray-400  py-3 px-9 relative overflow-clip rounded-3xl cursor-pointer mt-5"
+          onMouseEnter={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+        >
+          <motion.div
+            animate={{
+              scale: isHover ? 75 : 1,
+              backgroundColor: isHover ? "#22c55e" : "white",
+            }}
+            transition={{
+              duration: 0.2,
+              ease: "easeIn",
+            }}
+            className="w-[6px] h-[6px] rounded-full bg-black absolute left-8"
+          ></motion.div>
+          <motion.div
+            animate={{
+              x: isHover ? -8 : 8,
+              color: isHover ? "white" : "black",
+            }}
+            className="relative z-10"
+          >
+            <p className="font-Poppins text-xs md:text-sm font-semibold">
+              Explore More
+            </p>
+          </motion.div>
+          <motion.div className="flex  items-center absolute right-2">
+            <HiArrowRight color="white" />
+          </motion.div>
+        </div>
+        {/* <button className="w-40 h-10 bg-[#654321] text-white my-5 text-sm font-semibold font-Poppins">
           Explore More
-        </button>
+        </button> */}
       </section>
-      <div className="w-2/3"></div>
+      <div className="w-2/3 h-full relative md:-top-10 md:static">
+        <img
+          src={heroImage}
+          alt="peoples gathering in shopping"
+          className="w-full h-full object-cover"
+        />
+      </div>
     </div>
   )
 }
