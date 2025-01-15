43c14019096e158cf9e5e2f6dbc588db918fb189
sami855-ux <samitale56@outlook.com>
2025-01-15 10:06:59 +0500

    Making the header responsives

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -4,13 +4,13 @@ import logo from "../../assets/logo.png"
 
 export default function Header() {
   return (
-    <div className="w-full h-[75px] bg-gray-100 md:w-[70vw] md:absolute md:top-4 md:mx-auto md:left-48 md:rounded-3xl flex px-12 items-center justify-between">
+    <div className="w-full h-[75px] bg-white md:w-[78vw] lg:w-[85vw] absolute md:top-4 md:mx-auto md:left-40 md:rounded-3xl flex px-12 md:px-4 items-center justify-between">
       <section className="h-full w-32">
         <img src={logo} alt="image for e-commerce" className="w-28 h-full" />
       </section>
 
       {/* larger screen menu */}
-      <ul className="w-fit h-full flex items-center gap-5">
+      <ul className="hidden w-fit h-full md:flex items-center gap-7">
         <Link text="Home" />
         <Link text="Store" />
         <Link text="Blog" />
@@ -18,7 +18,7 @@ export default function Header() {
         <Link text="Contact" />
       </ul>
 
-      <button className="w-40 h-12 rounded-3xl text-sm bg-green-500">
+      <button className="hidden md:block w-40 h-12 rounded-3xl text-sm bg-green-500">
         Free consultation
       </button>
     </div>
