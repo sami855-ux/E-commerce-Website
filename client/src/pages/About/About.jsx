cc158f734eb0a340f2aff36c715be78e6ddc98cd
sami855-ux <samitale56@outlook.com>
2025-01-16 10:19:47 +0500

    About page is modifed to contain main section of about page

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,9 +1,12 @@
-import { Header } from "../../components"
+import { Footer, Header } from "../../components"
+import Main from "./Main"
 
 export default function About() {
   return (
     <>
       <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
