b898f1b11d0dc5f29cd8790e26c11f7f2a84e109
sami855-ux <samitale56@outlook.com>
2025-01-16 20:01:19 +0500

    Store page created

---
+++ client/src/pages/Store/Store.jsx
@@ -0,0 +1,12 @@
+import { Footer, Header } from "../../components"
+import Main from "./Main"
+
+export default function Store() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
