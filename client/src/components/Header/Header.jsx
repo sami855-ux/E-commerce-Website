f6c4f839cb3082d64e4c2958c22e0662f43eec46
sami855-ux <samitale56@outlook.com>
2025-01-17 13:27:53 +0500

    Some modification is added

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react"
-import PropTypes from "prop-types"
 import { motion, useScroll, useMotionValueEvent } from "framer-motion"
 import { NavLink } from "react-router-dom"
+import PropTypes from "prop-types"
+import { FaCartShopping } from "react-icons/fa6"
 
 import logo from "../../assets/logo.png"
+import CartView from "../Cartview/CartView"
 
-export default function Header() {
-  const { scrollY } = useScroll()
+export default function Header({ items = [] }) {
+  const [isClicked, setIsClicked] = useState(false)
   const [hidden, setHidden] = useState(false)
+  const { scrollY } = useScroll()
 
   useMotionValueEvent(scrollY, "change", (latest) => {
     const previous = scrollY.getPrevious()
@@ -20,36 +23,50 @@ export default function Header() {
   })
 
   return (
-    <motion.div
-      variants={{
-        visible: { y: 0 },
-        hidden: { y: "-120%" },
-      }}
-      animate={hidden ? "hidden" : "visible"}
-      transition={{
-        duration: 0.35,
-        ease: "easeInOut",
-      }}
-      className="w-full h-[75px] bg-[#f1f1f1] md:w-[78vw] lg:w-[85vw] sticky z-50 md:top-4 md:mx-auto md:left-40 md:rounded-3xl flex px-12 md:px-4 items-center justify-between"
-    >
-      <section className="w-32 h-full">
-        <img src={logo} alt="image for e-commerce" className="h-full w-28" />
-      </section>
+    <>
+      <motion.div
+        variants={{
+          visible: { y: 0 },
+          hidden: { y: "-120%" },
+        }}
+        animate={hidden ? "hidden" : "visible"}
+        transition={{
+          duration: 0.35,
+          ease: "easeInOut",
+        }}
+        className="w-full h-[75px] bg-[#f1f1f1] md:w-[78vw] lg:w-[85vw] sticky z-30 md:top-4 md:mx-auto md:left-40 md:rounded-3xl flex px-12 md:px-4 items-center justify-between"
+      >
+        <section className="w-32 h-full">
+          <img src={logo} alt="image for e-commerce" className="h-full w-28" />
+        </section>
 
-      {/* larger screen menu */}
-      <ul className="items-center hidden h-full w-fit md:flex gap-7">
-        <Link text="Home" to="/" />
-        <Link text="Store" to="/store" />
-        <Link text="Blog" to="/blog" />
-        <Link text="About" to="/about" />
-        <Link text="Contact" to="/contact" />
-      </ul>
+        {/* larger screen menu */}
+        <ul className="items-center hidden h-full w-fit md:flex gap-7">
+          <Link text="Home" to="/" />
+          <Link text="Store" to="/store" />
+          <Link text="Blog" to="/blog" />
+          <Link text="About" to="/about" />
+          <Link text="Contact" to="/contact" />
+        </ul>
 
-      <button className="hidden w-40 h-12 text-sm bg-green-500 md:block rounded-3xl">
-        Free consultation
-      </button>
-    </motion.div>
+        <span
+          className="w-10 h-10 rounded-full flex items-center justify-center bg-lightGreen cursor-pointer"
+          onClick={() => setIsClicked(true)}
+        >
+          <FaCartShopping color="white" />
+        </span>
+      </motion.div>
+      {isClicked ? (
+        <>
+          <CartView onIsClicked={setIsClicked} items={items} />
+        </>
+      ) : null}
+    </>
   )
+}
+
+Header.propTypes = {
+  items: PropTypes.array,
 }
 
 const Link = ({ text, to }) => {
--- client/src/pages/Store/Main.jsx
+++ client/src/pages/Store/Main.jsx
@@ -1,3 +1,35 @@
 export default function Main() {
-  return <div>main</div>
+  return (
+    <div className="w-full min-h-[200vh] pt-5 flex gap-4">
+      <section className="w-72 h-[80vh] rounded-md py-3 flex items-center flex-col px-2">
+        <form className="w-full mb-7">
+          <input
+            type="text "
+            placeholder="Search for item..."
+            className="w-full h-10 text-sm px-1 outline-none"
+          />
+        </form>
+
+        <div className="w-full ">
+          <h2 className="text-sm font-semibold text-gray-800 pb-5">Items</h2>
+          <p className="text-sm text-gray-700 mb-3 cursor-pointer pl-3">
+            Cloths
+          </p>
+          <p className="text-sm text-gray-700 mb-3 cursor-pointer pl-3">
+            Cosmetics
+          </p>
+          <p className="text-sm text-gray-700 mb-3 cursor-pointer pl-3">
+            Shoes
+          </p>
+          <p className="text-sm text-gray-700 mb-3 cursor-pointer pl-3">
+            Electronics
+          </p>
+          <p className="text-sm text-gray-700 mb-3 cursor-pointer pl-3">
+            Others
+          </p>
+        </div>
+      </section>
+      <section className="w-[80vw] min-h-screen py-10">Sami</section>
+    </div>
+  )
 }
