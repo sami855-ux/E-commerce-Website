ee494b166a99eed92dfef10436469bea67072e48
sami855-ux <samitale56@outlook.com>
2025-01-17 16:20:32 +0500

    Loader for the image is modifed

--- client/src/ui/LoaderImg/Loader.jsx
+++ client/src/ui/LoaderImg/Loader.jsx
@@ -2,7 +2,7 @@ import styles from "./loader.module.css"
 
 export default function Loader() {
   return (
-    <div className={styles.loading}>
+    <div className={`${styles.loading} relative left-[440px] top-40`}>
       <span></span>
       <span></span>
       <span></span>
