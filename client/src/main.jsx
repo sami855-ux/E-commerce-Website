775ce15324517ef2bd6fad33070386b73b660aa6
sami855-ux <samitale56@outlook.com>
2025-01-19 20:54:46 +0500

    Store added to the website

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { Provider } from "react-redux"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
 import "./index.css"
 import App from "./App.jsx"
+import { store } from "./StateMangament/Store.jsx"
 import About from "./pages/About/About.jsx"
 import Contact from "./pages/Contact/Contact.jsx"
 import Store from "./pages/Store/Store.jsx"
@@ -20,6 +22,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 )
