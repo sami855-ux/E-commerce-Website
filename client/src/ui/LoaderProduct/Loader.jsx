7b68a23a950fed917f3e07bf3b075047fb6317ff
sami855-ux <samitale56@outlook.com>
2025-01-20 11:22:05 +0500

    New loader for the image is added

--- client/src/ui/LoaderProduct/Loader.jsx
+++ client/src/ui/LoaderProduct/Loader.jsx
@@ -3,7 +3,7 @@ import styles from "./Loader.module.css"
 export default function Loader() {
   return (
     <div className={`${styles.newtons_cradle}`}>
-      <div className={styles.newtonscradle__dot}></div>
+      <div className={styles.newtons_cradle__dot}></div>
       <div className={styles.newtons_cradle__dot}></div>
       <div className={styles.newtons_cradle__do}></div>
       <div className={styles.newtons_cradle__dot}></div>
