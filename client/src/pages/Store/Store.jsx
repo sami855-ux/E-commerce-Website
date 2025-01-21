5daf0a5e051670e62dd54f965207c49bd0da9518
sami855-ux <samitale56@outlook.com>
2025-01-21 08:47:41 +0500

    scroll effect is added

--- client/src/pages/Store/Store.jsx
+++ client/src/pages/Store/Store.jsx
@@ -1,7 +1,15 @@
+import { useLocation } from "react-router-dom"
 import { Footer, Header } from "../../components"
 import Main from "./Main"
+import { useEffect } from "react"
 
 export default function Store() {
+  const location = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" })
+  }, [location])
+
   return (
     <>
       <Header />
